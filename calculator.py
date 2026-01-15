6290d2a401878fbcf12382955d1668336a86dbf2
ChaseWood <68968302+ChaseWood@users.noreply.github.com>
2026-01-14 18:48:38 -0700

    Adding our simple calculator file!

---
+++ calculator.py
@@ -0,0 +1,13 @@
+print("Enter 2 numbers that you want to add and subtract")
+
+num1 = int(input())
+
+num2 = int(input())
+
+addition = num1 + num2
+
+division = num1 / num2
+
+print("Your numbers added together = ", addition)
+
+print("Your numbers divided = ", division)
